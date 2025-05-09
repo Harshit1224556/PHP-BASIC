16524590aeab6ec9e7aa147d0fcbc7ff9593a70f
Harshit Kumar <150434886+Harshit1224556@users.noreply.github.com>
2025-05-10 03:24:40 +0530

    Add files via upload

---
+++ second.php
@@ -0,0 +1,37 @@
+<?php
+
+// echo "My car is <br>";
+// echo "My house is <br>";
+// echo "My boat is <br>";
+
+$a = 20;
+$b = 10.000;
+// echo "the sum is ".$a+$b;
+// echo "<br>";
+// echo "the difference is ".$a-$b;
+// echo "<br>";
+// echo "the multipication  is ".$a*$b;
+// echo "<br>";
+// echo "the division  is ".$a/$b;
+// echo "<br>";
+// echo "the division  is ".$a**$b;
+// if($a%2==0){
+//     echo "The number is even";
+// }
+// else{
+//     echo "The number is odd";
+// }
+
+
+//var_dump($a);
+
+
+if(is_int($a)){
+    echo "the number is an integer";
+}
+
+echo "<br>";
+if(is_float($b)){
+    echo "the number is  a float";
+}
+?> 
