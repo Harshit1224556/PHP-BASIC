b57d2d267d1c8e4146ab07decb6a8b5713cfa020
Harshit Kumar <150434886+Harshit1224556@users.noreply.github.com>
2025-05-10 03:30:28 +0530

    Add files via upload

---
+++ arraystr.php
@@ -0,0 +1,38 @@
+<?php
+$courses["INT220"] = "PHP";
+$courses["INT221"] = "PHP";
+$courses["INT222"] = "PHP";
+
+//printing array structure 
+print_r($courses);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
