320d5577e50664768f0bd6f243c93ea340353f0a
Harshit Kumar <150434886+Harshit1224556@users.noreply.github.com>
2025-05-10 03:25:10 +0530

    Add files via upload

---
+++ etp.php
@@ -0,0 +1,32 @@
+<?php
+
+// $a = 10;
+// $b = 20;
+// $c = $a + $b;
+// echo "Sum: $c";
+
+
+// $n = 123;
+// $rev = 0;
+// while($n!=0){
+
+//     $k = $n%10;
+//     $rev = $rev*10 + $k;
+//     $n=intdiv($n,10);
+// }
+
+// echo "Reverse of the given number is: $rev";
+
+
+// $fruits = array("mango","apple","orange");
+// foreach($fruits as $fruit){
+//     echo $fruit;
+// }
+
+// $students = array("john"=>85,"harshit"=>90,"Koo"=>123);
+// foreach($students as $name => $marks){
+//     echo "student: $name and marks: $marks\n";
+// }
+
+
+?>
